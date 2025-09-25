e3b5bd334f8cfd408b34dc72c08ee56c489d1c3e
Mohamad Fahrudin <mohamad.fahrudin18@gmail.com>
2025-09-25 19:31:44 +0700

    Time: 439 ms (80.99%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+select 
+    EmployeeUNI.unique_id, 
+    Employees.name
+from 
+    Employees
+left join 
+    EmployeeUNI on Employees.id = EmployeeUNI.id;
