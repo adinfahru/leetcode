8849a082aadb9a8060f45eacc57a0f0b3a627bb0
Mohamad Fahrudin <mohamad.fahrudin18@gmail.com>
2025-09-27 10:22:29 +0700

    Refine SQL query for user login timestamps
    Updated SQL query to use BETWEEN for date range and added ordering.

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -3,3 +3,9 @@ select user_id, max(time_stamp) as last_stamp
 from Logins
 where time_stamp >= '2020-01-01' and time_stamp < '2021-01-01'
 group by user_id
+
+-- Write your PostgreSQL query statement below
+SELECT user_id, MAX(time_stamp) as last_stamp FROM Logins 
+WHERE time_stamp BETWEEN '2020-01-01 00:00:00' AND '2020-12-31 23:59:59'
+GROUP BY user_id
+ORDER BY user_id DESC
