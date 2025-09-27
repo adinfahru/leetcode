f25934659bf5ad9805f55e904d877b2089b6e73b
Mohamad Fahrudin <mohamad.fahrudin18@gmail.com>
2025-09-27 09:58:25 +0700

    Time: 318 ms (10.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select u.name, sum(t.amount) as balance
+from Users as u
+join Transactions as t
+on u.account = t.account
+group by u.name
+having sum(t.amount) > 10000
