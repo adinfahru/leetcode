0fb6fd63d490f21368027a6493070918500a6d86
adinfahru <mohamad.fahrudin18@gmail.com>
2026-01-22 14:54:46 +0700

    Time: 421 ms (76.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,19 @@
+/* Write your T-SQL query statement below */
+
+-- filter yang primary flag nya Y
+-- kalo yang cuma satu berarti distinct trus ambil aja, gausah peduli primary flagnya
+-- lalu gabungin datanya
+
+
+select 
+	employee_id,
+	department_id
+from Employee
+where primary_flag = 'Y'
+union
+select
+	employee_id,
+	max(department_id) as department_id
+from Employee
+group by employee_id
+having count(primary_flag) = 1
