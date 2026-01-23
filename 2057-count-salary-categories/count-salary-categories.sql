27baea0f88d437a8f098513dd50d4895a676d7bc
adinfahru <mohamad.fahrudin18@gmail.com>
2026-01-23 15:56:27 +0700

    Time: 11960 ms (14.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+
+SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count FROM accounts WHERE income < 20000
+UNION
+SELECT 'Average Salary', COUNT(*) FROM accounts WHERE income BETWEEN 20000 AND 50000
+UNION
+SELECT 'High Salary', COUNT(*) FROM accounts WHERE income > 50000;
