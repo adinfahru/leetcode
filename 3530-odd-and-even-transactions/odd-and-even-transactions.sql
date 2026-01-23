3b5aebdc98beb71b0b80481c3f621d2052aa3c43
adinfahru <mohamad.fahrudin18@gmail.com>
2026-01-23 09:31:25 +0700

    Time: 311 ms (23.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3530-odd-and-even-transactions/odd-and-even-transactions.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+
+select transaction_date, 
+	sum(case when amount % 2 != 0 then amount else 0 end) odd_sum,
+	sum(case when amount % 2 = 0 then amount else 0 end) even_sum
+from transactions
+group by transaction_date
+order by transaction_date
