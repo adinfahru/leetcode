6ca0e51ab2c8fa27fcc567dfc1b8444b53cd2db2
adinfahru <mohamad.fahrudin18@gmail.com>
2026-03-06 10:03:48 +0700

    Time: 245 ms (36.09%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,2 +1,6 @@
 -- Write your PostgreSQL query statement below
-select p.product_id from Products as p where p.low_fats = 'Y' AND p.recyclable ='Y';
+
+SELECT 
+	product_id
+FROM products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
