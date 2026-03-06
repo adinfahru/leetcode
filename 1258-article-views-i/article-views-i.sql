c26d45f825cfafa476078cb88a0191e58a6f5980
adinfahru <mohamad.fahrudin18@gmail.com>
2026-03-06 11:02:52 +0700

    Time: 339 ms (75.93%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,6 @@
 -- Write your PostgreSQL query statement below
-select distinct author_id as id
-from Views
-where author_id = viewer_id
-order by author_id;
+
+SELECT DISTINCT
+    author_id as id
+FROM Views
+WHERE author_id = viewer_id
