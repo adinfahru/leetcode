acb95f545b57570080c1a1bd415c7d6da12f23d5
adinfahru <mohamad.fahrudin18@gmail.com>
2026-03-06 10:36:39 +0700

    Time: 292 ms (77.56%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,3 +1,9 @@
-select name, population, area
-from World
-where area >= 3000000 OR population >= 25000000
+-- Write your PostgreSQL query statement below
+
+SELECT 
+    name,
+    population,
+    area
+FROM World
+WHERE area >= 3000000 
+    OR population >= 25000000
