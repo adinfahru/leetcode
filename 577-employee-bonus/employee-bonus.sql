0367fbc0f88396903b898a8f44ddbdb832a890f8
adinfahru <mohamad.fahrudin18@gmail.com>
2026-03-09 13:40:48 +0700

    Time: 285 ms (97.59%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,9 @@
-SELECT name, bonus
-FROM Employee as e
-LEFT JOIN Bonus as b
-ON e.empId = b.empId
-WHERE bonus < 1000 or bonus is null;
+-- Write your PostgreSQL query statement below
+
+SELECT
+	name,
+	bonus 
+FROM employee e
+LEFT JOIN bonus b
+	ON e.empId = b.empid
+WHERE bonus < 1000 OR bonus IS null
