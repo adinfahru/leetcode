6a0e2253d5caf9580f74ac6d3ff6954ea459437c
adinfahru <mohamad.fahrudin18@gmail.com>
2026-03-17 11:55:14 +0700

    Time: 179 ms (78.78%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,11 @@
 -- Write your PostgreSQL query statement below
-select id, movie, description, rating
-from Cinema
-where description != 'boring' and id % 2 = 1
-order by rating desc
+
+SELECT 
+    id, 
+    movie,
+    description,
+    rating
+FROM Cinema
+WHERE id % 2 != 0 
+    and description != 'boring'
+ORDER BY rating desc
